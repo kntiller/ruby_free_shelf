ab266e1dce0e81e6f74d03a5979ec7f1947fb545
Kasey Tiller <kntiller@ccicustom.com>
2019-09-20 16:29:03 -0400

    Generate Cateogry Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_202643) do
+ActiveRecord::Schema.define(version: 2019_09_20_202819) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2019_09_20_202643) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
